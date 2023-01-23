e5939a4b8cb0314323fdec26cd92f5f20c173382
Stian Lystad <Stianlystad_1@hotmail.com>
2023-01-23 09:24:17 +0000

    added navbar

--- api-react/src/App.js
+++ api-react/src/App.js
@@ -1,12 +1,12 @@
 import Button from 'react-bootstrap/Button';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello world</h1>
-      <Button variant="primary">Primary</Button>
+      <NavBar />
     </div>
   );
 }
