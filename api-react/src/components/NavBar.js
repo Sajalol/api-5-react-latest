70fbf3048f714a371cea47f1e38f99032389f9a1
Stian Lystad <Stianlystad_1@hotmail.com>
2023-01-24 12:52:55 +0000

    added routing and more to navbar

--- api-react/src/components/NavBar.js
+++ api-react/src/components/NavBar.js
@@ -2,22 +2,43 @@ import React from 'react'
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 import logo from "../assets/api.png"
+import styles from '../styles/NavBar.module.css'
+import { NavLink } from 'react-router-dom';
 
 const NavBar = () => {
   return (
-    <Navbar bg="light" expand="md" fixed="top">
+    <Navbar className={styles.NavBar} expand="md" fixed="top">
     <Container>
+    <NavLink to="/">
       <Navbar.Brand>
         <img src={logo} alt="logo" height="45" />
       </Navbar.Brand>
+      </NavLink>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="ms-auto text-left">
-          <Nav.Link><i class="fa-solid fa-house"></i> Home</Nav.Link>
-          <Nav.Link><i class="fa-solid fa-right-to-bracket"></i>Sign In</Nav.Link>
-          <Nav.Link><i class="fa-solid fa-right-from-bracket"></i>Sign Out</Nav.Link>
+          <NavLink 
+          exact
+          className={styles.NavLink} 
+          activeClassName={styles.Active} 
+          to="/">
+            <i class="fa-solid fa-house"></i> Home
+            </NavLink>
+          <NavLink 
+          exact
+          className={styles.NavLink} 
+          activeClassName={styles.Active} 
+          to="/signin">
+            <i class="fa-solid fa-right-to-bracket"></i>Sign In
+            </NavLink>
+          <NavLink 
+          exact
+          className={styles.NavLink} 
+          activeClassName={styles.Active} 
+          to="/signout">
+            <i class="fa-solid fa-right-from-bracket"></i>Sign Out
+            </NavLink>
         </Nav>
       </Navbar.Collapse>
     </Container>
